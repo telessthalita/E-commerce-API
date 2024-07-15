68819c3eb5116471ea684356721197fd0b1896f7
Thalita Teles <telessthalita@gmail.com>
2024-07-14 21:45:53 -0300

    feat: Adiciona os dados

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -6,15 +8,28 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+CORS(app)
 
-#Produto (id, name, price, description)
+#Usuários 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=True)
+
+#Produtos 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+app.route('/login', method=["POST"])
+def login():
+    data = request.json
 
+    user = User.query.filter_by(username=data.get("username")).first()    
+    print(user)
+    return jsonify({"message": "Logged in successfully"})
 @app.route('/api/products/add', methods=["POST"])
 def add_product():
     data = request.json
@@ -34,8 +49,52 @@ def delete_product(product_id):
         return jsonify({"message": "Product deleted successfully."})
     return jsonify({"message": "Product not found."}), 400
 
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description,
+        })
+    return jsonify({"message": "Product not found"}), 404
 
-# Definir uma rota raiz (pg inicial) e a função que será executada ao requisitar
+@app.route('/api/products/<int:product_id>', methods=["PUT"])
+def update_product(product_id):
+    product = product.query.get(product_id)
+    if not product:
+        return jsonify ({"message": "Product not found"}), 404
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+        
+    if 'price' in data:
+        product.name = data['price']
+
+    if 'description' in data:
+        product.name = data['description']
+
+    return jsonify ({"message:" "Product update successfully"})
+
+@app.route('/api/products', methods=['GET'])
+def get_products( ):
+    products = Product.query.all()
+    product_list = []
+    print (products)
+    for product in products:
+        produc_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description,
+        }
+        product_list.append(produc_data)
+    return jsonify (product_list)
+        
+   
+# Definir uma rota raiz  e a função que será executada ao requisitar
 @app.route('/')
 def hello_world():
     return 'Hello World'
